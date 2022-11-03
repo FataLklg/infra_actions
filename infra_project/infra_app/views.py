3d4b2aabfe84ea3c01681f03c9951a9e59ab8821
Антон Тарасов <fatalklg@yandex.ru>
2022-11-03 19:39:49 +0300

    view.py update

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось! В сотый то раз не могло не получиться)')
 
 
 def second_page(request):
